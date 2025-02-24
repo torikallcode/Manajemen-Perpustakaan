0b135535644bcefc766433e84608d66bf7530cec
torikallcode <torikalcontent@gmail.com>
2025-02-24 21:22:07 +0800

    create handler GetBook

--- backend/handlers/book_handler.go
+++ backend/handlers/book_handler.go
@@ -3,8 +3,12 @@ package handlers
 import (
 	"backend/databases"
 	"backend/models"
+	"database/sql"
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func GetBooks(w http.ResponseWriter, r *http.Request) {
@@ -29,4 +33,27 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(books)
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
+
+	var book models.Book
+
+	query := "SELECT book_id, title, author, publication_year, genre, total_copies FROM books WHERE book_id = ?"
+	err = databases.DB.QueryRow(query, id).Scan(&book.Book_id, &book.Title, &book.Author, &book.Publication_year, &book.Genre, &book.Total_copies)
+	if err == sql.ErrNoRows {
+		http.Error(w, "rows not found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(book)
 }
