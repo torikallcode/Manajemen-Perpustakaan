02fb3e343e12688798f74cc0f8dc7f085b6921a0
torikallcode <torikalcontent@gmail.com>
2025-02-25 21:43:44 +0800

    update models book

--- backend/models/book.go
+++ backend/models/book.go
@@ -7,4 +7,12 @@ type Book struct {
 	Publication_year int    `json:"publication_year"`
 	Genre            string `json:"genre"`
 	Total_copies     int    `json:"total_copies"`
+	Isbn             int    `json:"isbn"`
+	Language         string `json:"language"`
+	Shelf_location   string `json:"shelf_location"`
+	Status           string `json:"status"`
+	Publisher        string `json:"publisher"`
+	Edition          string `json:"edition"`
+	Page_count       int    `json:"page_count"`
+	Cover_image_url  string `json:"cover_image_url"`
 }
